cf1fdf6967b04ef3e03f163132051472ce02c49b
Huan Bispo <huanaraujo@hotmail.com>
2020-08-22 11:50:16 -0300

    TaxComputation - Interface & Implementation

---
+++ PayCompute.Services/Implementation/TaxService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayCompute.Services.Implementation
+{
+    public class TaxService : ITaxService
+    {
+        private decimal taxRate;
+        private decimal tax;
+        // UK Tax Gov 
+        //https://www.gov.uk/guidance/rates-and-thresholds-for-employers-2019-to-2020#tax-thresholds-rates-and-codes
+        // Our computation is based on a monthly payment
+        public decimal TaxAmount(decimal totalAmount)
+        {
+            if (totalAmount <= 1042)
+            {
+                //Tax Free Rate
+                taxRate = .0m;
+                tax = totalAmount * taxRate;
+            }
+            else if (totalAmount > 1042 && totalAmount <= 3125)
+            {
+                //Basic tax Rate
+                taxRate = .20m;
+
+                //Income tax
+                tax = (1042 * .0m) + ((totalAmount - 1042) * taxRate);
+            }
+            else if (totalAmount > 3125 && totalAmount <= 12500)
+            {
+                //Higher tax Rate
+                taxRate = .40m;
+
+                //Income tax
+                tax = (1042 * .0m) + ((3125 - 1042) * .20m) + ((totalAmount - 3125) * taxRate);
+            }
+            else if (totalAmount > 12500)
+            {
+                //Additional tax Rate
+                taxRate = .45m;
+
+                //Income tax
+                tax = (1042 * .0m) + ((3125 - 1042) * .20m) + ((12500 - 3125) * .40m) + ((totalAmount - 12500) * taxRate);
+            }
+
+            return tax;
+        }
+    }
+}
