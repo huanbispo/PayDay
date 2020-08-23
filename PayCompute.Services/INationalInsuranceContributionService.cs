6658ce2ae50fe66d9464b88e16e6ae3f862e9107
Huan Bispo <huanaraujo@hotmail.com>
2020-08-23 00:48:33 -0300

    NationalInsuranceContribution Service and Implementation

---
+++ PayCompute.Services/INationalInsuranceContributionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayCompute.Services
+{
+    public interface INationalInsuranceContributionService
+    {
+        decimal NIContribution(decimal totalAmount);
+
+
+    }
+}
